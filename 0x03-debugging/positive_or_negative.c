5bee87cbba787ef5d7d47077c20e45c07b37e5df
ayyoub <ayoublambaraachentih@gmail.com>
2023-03-23 01:38:46 -0700

    my commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,7 +11,10 @@
 void positive_or_negative(int i)
 {
 	if (i < 0)
-		printf("%d is negative", i);
+		printf("%d is negative\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
 	else
-		printf("%d is positive", i);
+		printf("0 is zero\n");
+
 }
