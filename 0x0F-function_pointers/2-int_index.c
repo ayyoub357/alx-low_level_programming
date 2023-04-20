a4717251d0ac5d030fb9725a3ef62c623d52cd5d
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-19 19:08:51 -0700

    0x0F.C-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,20 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-	return (-1);
-
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i< size)
+			{
+				if (cmp(array[i])) return (1);
+					return (i);
+
+				i++;
+			}		
+		}
 	}
 	return (-1);
 }
