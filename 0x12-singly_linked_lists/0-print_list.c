48372af1f76b183a8484f29b49c0e77e42ccbf3d
ayyoub <ayoublambaraachentih@gmail.com>
2023-04-27 16:32:48 -0700

    C-singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns a string's length
+ * @s: the string whose length should be checked
+ *
+ * Return: string length in integers
+ */
+ 
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - a connected list is printed
+ * @h: pointer to the first node
+ *
+ * Return: list length
+ */
+ 
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+'This is the project: 0x12.C-singly_linked_lists' 
